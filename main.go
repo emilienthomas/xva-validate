2936dd4d8750a7b2efb9d02a9497329af4b7f335
Emilien THOMAS <emilien.thomas@sqits.net>
2019-02-25 11:28:50 +0100

    Use goreleaser

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/emilienthomas/xva-validate/xva"
 )
 
+// Will be set by goreleaser
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 // Tests integrity of the xva file passed through --xva parameter.
 // Verbosity levels are:
 // * 0: Only prints errors and "xva file is invalid" when needed
@@ -17,8 +24,14 @@ import (
 func main() {
 	// Program parameters
 	xvaPtr := flag.String("xva", "backup.xva", "xva file")
-	verbosityPtr := flag.Uint("v", 0, "Verbosity level")
+	verbosityPtr := flag.Uint("verbose", 0, "Verbosity level")
+	versionPtr := flag.Bool("version", false, "print version and exit")
 	flag.Parse()
+
+	if *versionPtr {
+		fmt.Printf("xva-validate %v, commit %v, built at %v", version, commit, date)
+		os.Exit(0)
+	}
 
 	isValid, validationIssue, err := xva.Validate(*xvaPtr, *verbosityPtr)
 	if err != nil {
